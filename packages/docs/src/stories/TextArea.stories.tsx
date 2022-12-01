72faf5fdb9c57d6fc7d01a85e7ae376ec6671846
Bruno Luiz <brunoslvrlz13@gmail.com>
2022-12-01 11:26:47 -0300

    Add text area component

---
+++ packages/docs/src/stories/TextArea.stories.tsx
@@ -0,0 +1,33 @@
+import { StoryObj, Meta } from '@storybook/react'
+import { Box, Text, TextArea, TextAreaProps } from '@bl-ui/react/'
+
+export default {
+  title: 'Form/Text Area',
+  component: TextArea,
+  args: {},
+  decorators: [
+    (story) => {
+      return (
+        <Box
+          as="label"
+          css={{ display: 'flex', flexDirection: 'column', gap: '$2' }}
+        >
+          <Text size="sm">Observations</Text>
+          {story()}
+        </Box>
+      )
+    },
+  ],
+} as Meta<TextAreaProps>
+
+export const Primary: StoryObj<TextAreaProps> = {
+  args: {
+    placeholder: 'Add any observations',
+  },
+}
+
+export const Disable: StoryObj<TextAreaProps> = {
+  args: {
+    disabled: true,
+  },
+}
