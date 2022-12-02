4bc2d9b28c995f682a516541c945921228f7bdd2
Bruno Luiz <brunoslvrlz13@gmail.com>
2022-12-02 14:20:54 -0300

    Add controll - arg types at text component

--- packages/docs/src/stories/Text.stories.tsx
+++ packages/docs/src/stories/Text.stories.tsx
@@ -7,6 +7,29 @@ export default {
   args: {
     children:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi quasi placeat vel dolores neque veniam ea nesciunt culpa. Enim consequuntur at facilis iure doloribus asperiores animi corrupti deleniti beatae minima!',
+    sizes: 'md',
+  },
+  argTypes: {
+    sizes: {
+      options: [
+        'xxs',
+        'xs',
+        'sm',
+        'md',
+        'lg',
+        'xl',
+        '2xl',
+        '4xl',
+        '5xl',
+        '6xl',
+        '7xl',
+        '8xl',
+        '9xl',
+      ],
+      control: {
+        type: 'inline-radio',
+      },
+    },
   },
 } as Meta<TextProps>
 
